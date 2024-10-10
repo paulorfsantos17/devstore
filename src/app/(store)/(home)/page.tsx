05886d63874bef44906f8f87ad891932e142a2a4
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-10-10 11:01:12 -0300

    feat: add home

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,8 +1,70 @@
-export default async function Store() {
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default async function Home() {
   await new Promise((resolve) => setTimeout(resolve, 2000))
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
+      <Link
+        href="/"
+        className="group relative col-span-6 row-span-6 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+      >
+        <Image
+          className="transition-transform duration-500 group-hover:scale-105"
+          src="/camiseta-dowhile-2022.png"
+          width={860}
+          height={860}
+          quality={100}
+          alt=""
+        />
+
+        <div className="absolute bottom-28 right-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1">
+          <span className="truncate text-sm">Moletom AI SIDE</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="group relative col-span-3 row-span-3 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900 p-1"
+      >
+        <Image
+          className="transition-transform duration-500 group-hover:scale-105"
+          src="/moletom-ai-side.png"
+          width={860}
+          height={860}
+          quality={100}
+          alt=""
+        />
+        <div className="absolute bottom-10 right-10 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1">
+          <span className="truncate text-sm">Moletom AI SIDE</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="group relative col-span-3 row-span-3 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+      >
+        <Image
+          className="transition-transform duration-500 group-hover:scale-105"
+          src="/moletom-java.png"
+          width={860}
+          height={860}
+          quality={100}
+          alt=""
+        />
+        <div className="absolute bottom-10 right-10 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1">
+          <span className="truncate text-sm">Moletom AI SIDE</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129
+          </span>
+        </div>
+      </Link>
     </div>
   )
 }
